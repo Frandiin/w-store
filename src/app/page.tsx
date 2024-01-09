4e82a963470009145fa39ad2281f9e1a01734c86
frandin <wellingtonfrandin@hotmail.com>
2024-01-09 13:00:35 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="bg-blue-300 text-center uppercase text-red-400"> oi</div>
-  );
+  return <div> oi</div>;
 }
