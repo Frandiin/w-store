80246554e46e4d47b8b809c731e3c9d98d11dc70
frandin <wellingtonfrandin@hotmail.com>
2024-01-09 16:41:03 -0300

    feat: add first discount banner to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
   const { data } = useSession();
-  return <div>{data?.user?.name}</div>;
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt={""}
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
 }
